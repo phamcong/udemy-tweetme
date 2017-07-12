6f7da4398585b7a052d23761174d6097354bb0ff
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-12 03:06:43 +0200

    S2L6: Django settings - Add a folder "settings" in tweetme. - In tweetme/settings, create 4 new files:   + __init__.py: Import configuration in base, production and local.   + local.py, base.py and production.py are copies of settings.py.   + Set Debug = False in production.py and change BASE_DIR (includes one more os.path.dirname)

--- tweetme/settings/production.py
+++ tweetme/settings/production.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q6jv94+)%)q&y)$!pd5^&*d+lx+h&d@t_2#ny2!cmz-&@s=jfi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
