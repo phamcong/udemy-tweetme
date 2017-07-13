4b129d81645afeeb7275faa7cd4f4540348b88fb
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-14 01:30:08 +0200

    S3L14: Querying the Database & Context - Import `Tweet` model into tweets/views.py. - Use `Tweet.objects.get()` and `Tweet.objects.all()` to request data. - Use `context` parameter in `render`.

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
 
 def tweet_detail_view(request, id=1):
-    return render(request, "detail_view.html", {})
+    obj = Tweet.objects.get(id=id),
+    context = {
+        "object": obj
+    }
+    return render(request, "detail_view.html", context)
 
 def tweet_list_view(request):
-    return render(request, "list_view.html", {})
+    queryset = Tweet.objects.all()
+    context = {
+        "object_list": queryset
+    }
+    return render(request, "list_view.html", context)
