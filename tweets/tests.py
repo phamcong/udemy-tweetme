2067e772bef8ae8e3ac28865eefa8ab558b14b56
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-19 01:43:57 +0200

    S3L32: Django Tests - Add test functions in tweets/tests.py.

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,3 +1,31 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+from .models import Tweet
+
+User = get_user_model()
+
+class TweetModelTestCase(TestCase):
+    def setUp(self):
+        some_random_user = User.objects.create(username='pcc1')
+
+    def test_tweet_item(self):
+        obj = Tweet.objects.create(
+            user = User.objects.first(),
+            content = "Some random content"
+        )
+        self.assertTrue(obj.content == "Some random content")
+        self.assertTrue(obj.id == 1)
+        absolute_url = reverse("tweet:detail", kwargs={"pk": 1})
+        self.assertTrue(absolute_url == obj.get_absolute_url())
+
+    def test_tweet_url(self):
+        obj = Tweet.objects.create(
+            user = User.objects.first(),
+            content = "Some random content"
+        )
+        absolute_url = reverse("tweet:detail", kwargs={"pk": obj.pk})
+        self.assertTrue(absolute_url == obj.get_absolute_url())
