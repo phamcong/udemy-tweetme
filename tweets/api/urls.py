fb5f92f867f652946fa0e4310969996dd157b1a2
Cong Cuong PHAM <ccuong.ph@gmail.com>
2017-07-21 04:48:18 +0200

    S3L37-L38-L39: jQuery Search, jQuery the Form and Ajax & Create API View: - Enable search and create functions for list view with data obtained from API.

--- tweets/api/urls.py
+++ tweets/api/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from .views import (
+    TweetCreateAPIView,
     TweetListAPIView
 )
 
 urlpatterns = [
     # url(r'^$', RedirectView.as_view(url="/")),
     url(r'^$', TweetListAPIView.as_view(), name='list'), # /api/tweet
-    # url(r'^create/$', TweetCreateView.as_view(), name='create'), # /tweet/create
+    url(r'^create/$', TweetCreateAPIView.as_view(), name='create'), # /api/tweet/create
     # # url(r'^create/$', tweet_create_view, name='create'),
     # url(r'^(?P<pk>\d+)/$', TweetDetailView.as_view(), name='detail'), # /tweet/1
     # url(r'^(?P<pk>\d+)/update/$', TweetUpdateView.as_view(), name='update'), # /tweet/1/update
